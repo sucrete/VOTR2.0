cd21a986e612611810e46ee81eff896b6584ded2
William Andree <williamandree@gmail.com>
2021-08-03 23:42:42 -0500

    further testing

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -10,7 +10,7 @@
         &#8220;First, solve the problem. Then, write the code.&#8221;
         <footer>
           <small>
-            <em>&mdash;John Johnson</em>
+            <em>&mdash;Jack Johnson</em>
           </small>
         </footer>
       </blockquote>
